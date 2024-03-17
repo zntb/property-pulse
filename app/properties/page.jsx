f06e93fedfbd85def66dfa09d7a13b66f0e13027
zntb <zntbdev@proton.me>
2024-03-17 14:38:20 +0200

    Add Requests File

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,19 +1,5 @@
 import PropertyCard from '../../components/PropertyCard.jsx';
-
-async function fetchProperties() {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
-
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-}
+import { fetchProperties } from '../../utils/requests.js';
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
