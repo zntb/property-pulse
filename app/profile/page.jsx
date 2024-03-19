143f93c6ce50e638ae6ac5e1a9273523051bbc70
zntb <zntbdev@proton.me>
2024-03-19 18:04:23 +0200

    Add Toast Notifications

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import profileDefault from '@/assets/images/profile.png';
 import Spinner from '@/components/Spinner.jsx';
+import { toast } from 'react-toastify';
 
 const ProfilePage = () => {
   const { data: session } = useSession();
@@ -61,13 +62,13 @@ const ProfilePage = () => {
 
         setProperties(updatedProperties);
 
-        alert('Property deleted successfully');
+        toast.success('Property deleted successfully');
       } else {
-        alert('Failed to delete property');
+        toast.error('Failed to delete property');
       }
     } catch (error) {
       console.log(error);
-      alert('Failed to delete property');
+      toast.error('Failed to delete property');
     }
   };
 
