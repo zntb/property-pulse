6de975216cdf470739d5b029a0b4cb20b73a1164
zntb <zenetibi@yahoo.com>
2024-03-20 18:15:31 +0200

    Add Saved Properties Page

---
+++ app/properties/saved/page.jsx
@@ -0,0 +1,54 @@
+'use client';
+import { useState, useEffect } from 'react';
+import PropertyCard from '@/components/PropertyCard.jsx';
+import Spinner from '@/components/Spinner.jsx';
+import { toast } from 'react-toastify';
+
+const SavedPropertiesPage = () => {
+  const [properties, setProperties] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchSavedProperties = async () => {
+      try {
+        const res = await fetch('/api/bookmarks');
+        if (res.status === 200) {
+          const data = await res.json();
+
+          setProperties(data);
+        } else {
+          console.log(res.statusText);
+          toast.error('Failed to fetch saved properties');
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error('Failed to fetch saved properties');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchSavedProperties();
+  }, []);
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <section className="px-4 py-6">
+      <h1 className="text-2xl font-bold mb-4">Saved Properties</h1>
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No saved properties</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default SavedPropertiesPage;
