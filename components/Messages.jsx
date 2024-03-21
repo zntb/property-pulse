cb456df908f28f3dfc1d62109a82f311878565b9
zntb <zenetibi@yahoo.com>
2024-03-21 17:30:05 +0200

    Display Messages

--- components/Messages.jsx
+++ components/Messages.jsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import Spinner from './Spinner.jsx';
+import Message from './Message.jsx';
 
 const Messages = () => {
   const [messages, setMessages] = useState([]);
@@ -23,7 +24,27 @@ const Messages = () => {
     gethMessages();
   }, []);
 
-  return <div>Messages</div>;
+  return loading ? (
+    <Spinner loading={loading} />
+  ) : (
+    <section className="bg-blue-50">
+      <div className="container m-auto py-24 max-w-6xl">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
+
+          <div className="space-y-4">
+            {messages.length === 0 ? (
+              <p>Tou have no messages.</p>
+            ) : (
+              messages.map((message) => (
+                <Message key={message._id} message={message} />
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Messages;
