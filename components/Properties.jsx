42882dcaff6a05a498b76bf6d43e0ee422e01b4a
zntb <zntbdev@proton.me>
2024-03-22 09:30:14 +0200

    Properties Client Component Refactor

---
+++ components/Properties.jsx
@@ -0,0 +1,50 @@
+'use client';
+import { useState, useEffect } from 'react';
+import PropertyCard from '@/components/PropertyCard.jsx';
+import Spinner from './Spinner.jsx';
+
+const Properties = () => {
+  const [properties, setProperties] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Fetch Properties
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const res = await fetch('/api/properties');
+
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const data = await res.json();
+        setProperties(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchProperties();
+  }, []);
+
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found.</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Properties;
