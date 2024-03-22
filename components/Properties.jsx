aaf8be30d6c9a857f4db9a81c5cc8981fc03b240
zntb <zenetibi@yahoo.com>
2024-03-22 09:54:41 +0200

    Implement Pagination

--- components/Properties.jsx
+++ components/Properties.jsx
@@ -6,19 +6,25 @@ import Spinner from './Spinner.jsx';
 const Properties = () => {
   const [properties, setProperties] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(3);
+  const [totalItems, setTotalItems] = useState(0);
 
   // Fetch Properties
   useEffect(() => {
     const fetchProperties = async () => {
       try {
-        const res = await fetch('/api/properties');
+        const res = await fetch(
+          `/api/properties?page=${page}&pageSize=${pageSize}`
+        );
 
         if (!res.ok) {
           throw new Error('Failed to fetch data');
         }
 
         const data = await res.json();
-        setProperties(data);
+        setProperties(data.properties);
+        setTotalItems(data.total);
       } catch (error) {
         console.log(error);
       } finally {
