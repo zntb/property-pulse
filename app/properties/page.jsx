f3a4ebe641a72c13fc24df49d932b48aa0ed7a2f
zntb <zenetibi@yahoo.com>
2024-03-22 16:36:52 +0200

    Fix errors, prepare to deploy

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -5,7 +5,7 @@ const PropertiesPage = async () => {
   return (
     <>
       <section className="bg-blue-700 py-4">
-        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+        <div className="mt-[4.25rem] max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
           <PropertySearchForm />
         </div>
       </section>
