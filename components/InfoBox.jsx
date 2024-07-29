86c75e34358be537062a092a2f25e59a230ef66e
zntb <zntbdev@proton.me>
2024-07-29 21:11:31 +0300

    Render properties from JSON file

--- components/InfoBox.jsx
+++ components/InfoBox.jsx
@@ -1,7 +1,7 @@
 const InfoBox = ({
   heading,
-  backgroundColor = 'bg-gray-100',
-  textColor = 'text-gray-800',
+  backgroundColor = "bg-gray-100",
+  textColor = "text-gray-800",
   buttonInfo,
   children,
 }) => {
@@ -11,7 +11,8 @@ const InfoBox = ({
       <p className={`${textColor} mt-2 mb-4`}>{children}</p>
       <a
         href={buttonInfo.link}
-        className={`inline-block ${buttonInfo.backgroundColor} text-white rounded-lg px-4 py-2 hover:opacity-80`}
+        className={`inline-block ${buttonInfo.backgroundColor} text-white rounded-lg px-4 py-2
+          hover:opacity-80`}
       >
         {buttonInfo.text}
       </a>
--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -1,29 +1,29 @@
-import InfoBox from './InfoBox';
+import InfoBox from "./InfoBox";
 
 const InfoBoxes = () => {
   return (
     <section>
-      <div className='container-xl lg:container m-auto'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <InfoBox
-            heading='For Renters'
-            backgroundColor='bg-gray-100'
+            heading="For Renters"
+            backgroundColor="bg-gray-100"
             buttonInfo={{
-              text: 'Browse Properties',
-              link: '/properties',
-              backgroundColor: 'bg-black',
+              text: "Browse Properties",
+              link: "/properties",
+              backgroundColor: "bg-black",
             }}
           >
             Find your dream rental property. Bookmark properties and contact
             owners.
           </InfoBox>
           <InfoBox
-            heading='For Property Owners'
-            backgroundColor='bg-blue-100'
+            heading="For Property Owners"
+            backgroundColor="bg-blue-100"
             buttonInfo={{
-              text: 'Add Property',
-              link: '/properties/add',
-              backgroundColor: 'bg-blue-500',
+              text: "Add Property",
+              link: "/properties/add",
+              backgroundColor: "bg-blue-500",
             }}
           >
             List your properties and reach potential tenants. Rent as an Airbnb
