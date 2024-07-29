76d389b7238a7f6cd374d83e8b5f708276e1f245
zntb <zenetibi@yahoo.com>
2024-07-29 20:54:13 +0300

    Initial setup

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,7 @@
-import Hero from '../components/Hero.jsx';
-import InfoBoxes from '../components/InfoBoxes.jsx';
-import HomeProperties from '../components/HomeProperties.jsx';
-import FeaturedProperties from '../components/FeaturedProperties.jsx';
-
 const HomePage = () => {
   return (
     <>
-      <Hero />
-      <InfoBoxes />
-      <FeaturedProperties />
-      <HomeProperties />
+      <h1>Home Page</h1>
     </>
   );
 };
