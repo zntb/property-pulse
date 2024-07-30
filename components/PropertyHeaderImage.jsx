fccda3bdaeb5f2295a906d14fae78baa60ba0cef
zntb <zntbdev@proton.me>
2024-07-30 09:27:44 +0300

    Property add and cloudinary upload

--- components/PropertyHeaderImage.jsx
+++ components/PropertyHeaderImage.jsx
@@ -1,17 +1,17 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const PropertyHeaderImage = ({ image }) => {
   return (
     <section>
-      <div className='container-xl m-auto'>
-        <div className='grid grid-cols-1'>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
           <Image
-            src={`/images/properties/${image}`}
-            alt=''
-            className='object-cover h-[400px] w-full'
+            src={image}
+            alt=""
+            className="object-cover h-[400px] w-full"
             width={0}
             height={0}
-            sizes='100vw'
+            sizes="100vw"
             priority={true}
           />
         </div>
