cb89bd26e9cb13e1b1d664f37b1560bfbf1373b0
zntb <zntbdev@proton.me>
2024-08-02 08:42:40 +0300

    Fix styles in message page, saved page, PropertyDetails and Navbar components, close the 'Profile dropdown' when clicking anywhere outside of it.

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -50,7 +50,10 @@ const MessagePage = async () => {
     .filter((message): message is IMessage => message !== null);
 
   return (
-    <section className="bg-blue-50">
+    <section
+      className="bg-blue-50"
+      style={{ minHeight: 'calc(100vh - 162px)' }}
+    >
       <div className="container m-auto py-24 max-w-6xl">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <h1 className="text-3xl font-bold mb-4">Your Messages</h1>
